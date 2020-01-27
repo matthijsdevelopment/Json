576f0d654698aefff606856c98af61e051dac40d
Matthijs Berghuis <Berghuismatthijs@gmail.com>
2020-01-27 16:28:58 +0100

    commits van video nr 5

--- sorteerboeken.js
+++ sorteerboeken.js
@@ -10,19 +10,39 @@ xmlhttp.open('GET', "boek.json"  , true);
 xmlhttp.send();
 
 
+const makeTableHead = (arr) => {
+    let head = '<table><tr>';
+    arr.forEach((item) => {
+        head += "<th>" + item + "</th>";
+    });
+    head += "</tr>";
+    return head;
+  }
+
+  const makeTableRow = (arr) => {
+    let row = '<table><tr>';
+    arr.forEach((item) => {
+        row += "<td>" + item + "</td>";
+    });
+    row += "</tr>";
+    return row;
+  }
+
 // Object dat de boeken uitvoert en sorteert.
 let sortBooksOBJ = {
     data: "",
 
     sorteren: function() {
         this.data.sort( (a,b) => a.auteur[0] > b.auteur[0] ? 1 : -1);
-        this.uitvoeren();
+        this.uitvoeren(this.data);
     },
 
-    uitvoeren: function() {
-        let uitvoer = "";
-        for ( let i=0; i<this.data.length; i++) {
-            uitvoer += this.data[i].auteur + "<br>";
+    uitvoeren: function(data) {
+        let uitvoer = makeTableHead(["titel", "auteur(s)", "cover", "uitgave", "bladzijden", "taal", "EAN"]);
+        for ( let i=0; i< data.length; i++) {
+            let imgElement = "<img src='" + data[i].cover +  "'>";
+            uitvoer += makeTableRow([data[i].titel, data[i].titel,imgElement,data[i].uitgave,data[i].paginas,data[i].taal,data[i].ean]);
+
         }
         document.getElementById('uitvoer').innerHTML = uitvoer;
     }
