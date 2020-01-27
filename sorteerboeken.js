43c69bd4e576027b1cb9b077c1471d2a9bd451c7
Matthijs Berghuis <Berghuismatthijs@gmail.com>
2020-01-27 14:12:11 +0100

    video 2

--- sorteerboeken.js
+++ sorteerboeken.js
@@ -1,14 +1,22 @@
-const url = "boek.json";
-
 let xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
     if(this.readyState==4 && this.status == 200) {
-       document.getElementById('uitvoer').innerHTML = this.responseText;
-   } else {
-       console.log("readystate " + this.readyState);
-       console.log("status " + this.status);
-   }
+       sortBooksOBJ.data = JSON.parse(this.responseText);
+       sortBooksOBJ.uitvoeren();
+   } 
 }
 
-xmlhttp.open('GET', url, true);
+xmlhttp.open('GET', "boek.json"  , true);
 xmlhttp.send();
+
+let sortBooksOBJ = {
+    data: "",
+
+    uitvoeren: function() {
+        let uitvoer = "";
+        for ( let i=0; i<this.data.length; i++) {
+            uitvoer += this.data[i].titel + "<br>";
+        }
+        document.getElementById('uitvoer').innerHTML = uitvoer;
+    }
+}
