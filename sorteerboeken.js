1d1169bed997c0f5dfcaf6a29bff91cdd0106c77
Matthijs Berghuis <Berghuismatthijs@gmail.com>
2020-01-27 17:53:33 +0100

    inserted all info from video 10
    created time and date sorting items

--- sorteerboeken.js
+++ sorteerboeken.js
@@ -2,13 +2,14 @@ let kenmerk = document.getElementById('kenmerk');
 kenmerk.addEventListener('change', (e) => {
     sortBooksOBJ.kenmerk = e.target.value;
     sortBooksOBJ.sorteren();
-})
+});
 
 // JSON importeren
 let xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
         sortBooksOBJ.data = JSON.parse(this.responseText);
+        sortBooksOBJ.insertJSdateIn();
         sortBooksOBJ.sorteren();
     }
 }
@@ -39,12 +40,70 @@ const makeTableRow = (arr, accent) => {
     return row;
 }
 
+const giveMonthNumber = (month) => {
+    let number;
+    switch (month) {
+        case "januari":
+          number = 0;
+          break;
+        case "februari":
+          number = 1;
+          break;
+        case "maart":
+          number = 2;
+          break;
+        case "april":
+          number = 3;
+          break;
+        case "mei":
+          number = 4;
+          break;
+        case "juni":
+          number = 5;
+          break;
+        case "juli":
+          number = 6;
+          break;
+        case "augustus":
+          number = 7;
+          break;
+        case "september":
+          number = 8;
+          break;
+        case "oktober":
+          number = 9;
+          break;
+        case "november":
+          number = 10;
+          break;
+        case "december":
+          number = 11;
+          break;
+        default:
+          number = 0
+    
+      }
+        return number
+}
+
+const makeJSdate = (monthYear) => {
+    let myArray = monthYear.split(" ");
+    let date = new Date(myArray[1], giveMonthNumber(myArray[0 ]));
+    return date ;
+}
+
 // Object dat de boeken uitvoert en sorteert.
 let sortBooksOBJ = {
     data: "",
-
+ 
     kenmerk: "titel",
      
+    insertJSdateIn: function() {
+        this.data.forEach((item) => {
+            item.jsDate = makeJSdate(item.uitgave)
+        })
+    },
+
 
     sorteren: function () {
         this.data.sort((a, b) => a["this.kenmerk"] > b["this.kenmerk "] ? 1 : -1);
